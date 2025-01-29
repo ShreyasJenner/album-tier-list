b02810f03a9441c33d8cd168e1c408beb97ad6af
Shreyas Jenner <shreyasjenner4@gmail.com>
2025-01-29 17:45:58 +0530

    updated next.config.js with url

--- next.config.js
+++ next.config.js
@@ -2,17 +2,15 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ["localhost"],
+    domains: ['localhost', 'https://album-tier-list-2zc87lry8-shreyasjenner4-gmailcoms-projects.vercel.app'],
     remotePatterns: [
       {
-        protocol: "http",
-        hostname: "localhost",
-        port: "3000",
-        pathname: "/api/upload/**",
+        protocol: 'https',
+        hostname: 'your-project-name.vercel.app',
+        pathname: '/api/upload/**',
       },
     ],
   },
 }
 
 module.exports = nextConfig
-
