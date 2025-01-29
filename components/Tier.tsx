bfb5a2a18d253d1d1091707360851d83d157d031
Shreyas Jenner <shreyasjenner4@gmail.com>
2025-01-29 17:50:44 +0530

    attempted second error fix

--- components/Tier.tsx
+++ components/Tier.tsx
@@ -41,7 +41,6 @@ export default function Tier({
             image={image}
             onImageMove={onImageMove}
             onToggleListened={onToggleListened}
-            currentTierId={id}
             isSelected={selection[image.id] || false}
             onToggleSelection={onToggleSelection}
             selection={selection}
